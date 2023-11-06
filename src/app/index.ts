dab9c88c2fa943119cec96477c30a06abe7a44e2
gnrfilipe04 <gnr_filipe@hotmail.com>
2023-11-06 20:56:41 -0300

    :sparkles: abstract methods

--- src/app/index.ts
+++ src/app/index.ts
@@ -16,19 +16,34 @@ export class App implements AppDTO {
 	}
 
 	async run(){
+		
+		await this.titlePageToFile()
+		
+	}
 
-		const { url, titleXPath, filename } = this.config
+	async titlePageToFile(){
+		const element = await this.getTitlePage()
+
+		const elementOrNotFound = element || 'Element not found!'
+
+		this.setTitleInFile({ element: elementOrNotFound })
+	}
+
+	async getTitlePage(){
+		const { url, titleXPath, } = this.config
 
 		const myReadPageFactory = await MyReadPageFactory.createInstance({ url, })
 		
 		const element = await myReadPageFactory.searchByXPath(titleXPath)
 
-		if(!element) throw new Error('Element not found')
+		return element
+	}
+
+	async setTitleInFile({ element, }: { element: string}){
+		const { filename, } = this.config
 
 		const fileFactory = await FileFactory.createInstance({ filename, })
 
 		fileFactory.write(element)
-    
-		console.log(element)
 	}
 }
